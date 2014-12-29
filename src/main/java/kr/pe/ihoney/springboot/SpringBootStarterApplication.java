eac77e3069101fde2df1648ad1607a1de92ad431
jiheon.kim <jiheon.kim@innotree.com>
2014-12-29 11:11:06 +0900

    #1 SpringBootStarterApplication 클래스 설명
    1.2.0. 부터 @SpringBootApplication 사용

--- src/main/java/kr/pe/ihoney/springboot/SpringBootStarterApplication.java
+++ src/main/java/kr/pe/ihoney/springboot/SpringBootStarterApplication.java
@@ -1,12 +1,15 @@
 package kr.pe.ihoney.springboot;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
+ * SpringBoot 1.2.0.Release 에서 큰 변화중 하나라면, 1.1.9까지는 3개의 애노테이션
+ * {@code @Configuration}, {@code @EnableAutoConfiguration} and
+ * {@code @ComponentScan}를 사용해야했지만, 1.2.0. 부터는 {@code @SpringBootApplication}
+ * 애노테이션 하나로 통일되었다는 것.
  * 
  * @author jiheon
  *
